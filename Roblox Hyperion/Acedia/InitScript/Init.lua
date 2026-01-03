ae2b48663e24280cbb2031450082f36ef6652500
BaconStorage <BaconStorage@hotmail.com>
2026-01-03 00:25:43 -0800

    Add files via upload

---
+++ Roblox Hyperion/Acedia/InitScript/Init.lua
@@ -0,0 +1,12 @@
+local mt = getrawmetatable(game)
+local old_index = mt.__index
+local old_namecall = mt.__namecall
+setreadonly(mt, false)
+mt.__namecall = function(self, ...)
+    local method = getnamecallmethod()
+    if method == "HttpGet" then
+        return getgenv().HttpGet(...)
+    end
+    return old_namecall(self, ...)
+end
+setreadonly(mt, true)
