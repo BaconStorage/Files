d5b3240be2bd392c4d361b529286f4f2512fa801
BaconStorage <BaconStorage@hotmail.com>
2026-01-03 05:02:25 -0800

    Update Init.lua

--- Roblox Hyperion/Acedia/InitScript/Init.lua
+++ Roblox Hyperion/Acedia/InitScript/Init.lua
@@ -2,11 +2,22 @@
 spawn(function()
 local Workspace = game:GetService("Workspace")
 
--- Now run your metatable hook
+
+
+
 local mt = getrawmetatable(game)
 local old_index = mt.__index
 local old_namecall = mt.__namecall
+
 setreadonly(mt, false)
+
+mt.__index = function(self, key)
+    if key == "HttpGet" then
+        return getgenv().HttpGet
+    end
+    return old_index(self, key)
+end
+
 mt.__namecall = function(self, ...)
     local method = getnamecallmethod()
     if method == "HttpGet" then
@@ -14,5 +25,6 @@ mt.__namecall = function(self, ...)
     end
     return old_namecall(self, ...)
 end
+
 setreadonly(mt, true)
     end)
