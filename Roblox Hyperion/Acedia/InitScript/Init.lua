5191d420d58ae09edcde699693d0327f2e91566d
BaconStorage <BaconStorage@hotmail.com>
2026-01-08 05:34:28 -0800

    Update Init.lua

--- Roblox Hyperion/Acedia/InitScript/Init.lua
+++ Roblox Hyperion/Acedia/InitScript/Init.lua
@@ -1,61 +1,39 @@
+-- Wait until Workspace is available
 
 spawn(function()
-    local real_HttpGet = getgenv().HttpGet
 
 
-    local real_GetObjects = getgenv().GetObjects
 
+local mt = getrawmetatable(getgenv().game)
+local old_index = mt.__index
+local old_namecall = mt.__namecall
 
-    -- Hook functions
-    local function hookHttpGet(self, url, ...)
-        print("[Acedia] HttpGet intercepted:", url)
-        return real_HttpGet(url, ...)
+setreadonly(mt, false)
+
+mt.__index = newcclosure(function(self, key)
+    if key == "HttpGet" or key == "HttpGetAsync" then
+        return getgenv().HttpGet
+    elseif key == "GetObjects" then
+        return getgenv().GetObjects
     end
+    return old_index(self, key)
+end)
 
-    local function hookGetObjects(self, assetId)
-        print("[Acedia] GetObjects intercepted:", assetId)
-        return real_GetObjects(assetId)
+mt.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+
+    if method == "HttpGet" or method == "HttpGetAsync" then
+        return getgenv().HttpGet(...)
+    elseif method == "GetObjects" then
+        return getgenv().GetObjects(...)
     end
+    return old_namecall(self, ...)
+end)
 
-    -- Get the metatable of game
-    local mt = getrawmetatable(getgenv().game)
-    if not mt then
-        warn("Could not get metatable of game")
-        return
-    end
+setreadonly(mt, true)
+    end)
 
-    -- Backup original __namecall if needed
-    local old_namecall = mt.__namecall
 
-    -- Make metatable writable
-    if make_writeable then
-        make_writeable(mt)  -- Some exploits have this
-    elseif setreadonly then
-        setreadonly(mt, false)
-    end
 
-    -- Hook __namecall to intercept method calls like game:HttpGet()
-    mt.__namecall = newcclosure(function(self, ...)
-        local method = getnamecallmethod()
-        local args = {...}
-
-        if self == game then
-            if method == "HttpGet" or method == "HttpGetAsync" then
-                return real_HttpGet(args[1], unpack(args, 2))
-            elseif method == "GetObjects" then
-                return real_GetObjects(args[1])
-            end
-        end
-
-        -- Fall back to original behavior
-        if old_namecall then
-            return old_namecall(self, ...)
-        end
-    end
-
-    end))
-if setreadonly then
-        setreadonly(mt, true)
-    end
 
 
